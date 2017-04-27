435218ab4ee259daa26e6861729d0d2c1bfa98fb
taozhaolib <tao.zhao.lib@gmail.com>
2017-04-27 16:06:14 -0500

    change command to mvn -v

--- libtoolsq/tasks/tasks.py
+++ libtoolsq/tasks/tasks.py
@@ -10,12 +10,13 @@ def libtooljournalpath(
         args: x and y
         return addition or concatination of strings
     """
-    cmd_tmp = "mvn exec:exec@journal-search -Ddata=\'{{\"journal-search\": {{\"id\" : \"{0}\", \"publisher\" : \"{1}\", \"startDate\": \"{2}\", \"endDate\" : \"{3}\", \"affiliate\" : \"{4}\"}}}}\' -f ./kernal-api/pom.xml"
-    cmd = cmd_tmp.format(id, publisher, startdate, enddate, affiliate)
+#    cmd_tmp = "mvn exec:exec@journal-search -Ddata=\'{{\"journal-search\": {{\"id\" : \"{0}\", \"publisher\" : \"{1}\", \"startDate\": \"{2}\", \"endDate\" : \"{3}\", \"affiliate\" : \"{4}\"}}}}\' -f ./kernal-api/pom.xml"
+#    cmd = cmd_tmp.format(id, publisher, startdate, enddate, affiliate)
 #    try:
 #        resp = check_output(cmd, shell=True)
 #    except CalledProcessError:
 #        return {"status": "error catched"}
+    cmd_tmp = "mvn -v"
     resp = check_output(cmd, shell=True)
     # if command returns just the path
     return resp

28dece1dd4da87f6c8b0ef41162e58513ef2f617
taozhaolib <tao.zhao.lib@gmail.com>
2017-05-01 15:24:58 -0500

    fixed the function call by changing queue name in cybercommon

--- libtoolsq/tasks/tasks.py
+++ libtoolsq/tasks/tasks.py
@@ -12,16 +12,16 @@ def libtooljournalpath(
         args: x and y
         return addition or concatination of strings
     """
-#    cmd_tmp = "mvn exec:exec@journal-search -Ddata=\'{{\"journal-search\": {{\"id\" : \"{0}\", \"publisher\" : \"{1}\", \"startDate\": \"{2}\", \"endDate\" : \"{3}\", \"affiliate\" : \"{4}\"}}}}\' -f /Users/zhao0677/Projects/shareokdata/kernel-api/pom.xml"
-#    cmd = cmd_tmp.format(id, publisher, startdate, enddate, affiliate)
-    cmd = "mvn -v"
-#    try:
-    resp = check_output(cmd, shell=True)
-#    except CalledProcessError:
-#        return {"status": "error catched"}
+    cmd_tmp = "mvn exec:exec@journal-search -Ddata=\'{{\"journal-search\": {{\"id\" : \"{0}\", \"publisher\" : \"{1}\", \"startDate\": \"{2}\", \"endDate\" : \"{3}\", \"affiliate\" : \"{4}\"}}}}\' -f /Users/zhao0677/Projects/shareokdata/kernel-api/pom.xml"
+    cmd = cmd_tmp.format(id, publisher, startdate, enddate, affiliate)
+#    cmd = "mvn -v"
+    try:
+        resp = check_output(cmd, shell=True)
+    except CalledProcessError:
+        return {"status": "error catched"}
 
     # if command returns just the path
-    print resp
+    return resp
 
     # else if path is last line in stdout
 #    return [line for line in resp.splitlines()][-1]

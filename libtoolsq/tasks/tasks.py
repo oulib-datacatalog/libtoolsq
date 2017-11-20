cb3f7f9ccc4b9fec52ace7769344132e8250e18b
taozhaolib <tao.zhao.lib@gmail.com>
2017-11-20 10:24:42 -0600

    added print for safpath for task safPackageGenerationAndImport

--- libtoolsq/tasks/tasks.py
+++ libtoolsq/tasks/tasks.py
@@ -19,6 +19,8 @@ def safPackageGenerationAndImport(
     #### Generating the SAF package ####
     safpath = safPackageGen(id, csvPath)
 
+    print '\n safpath = ' + safpath + ' for safPackageGenerationAndImport id = ' + id
+
     """
         "Import the DSpace SAF package into DSpace repository"
     """
